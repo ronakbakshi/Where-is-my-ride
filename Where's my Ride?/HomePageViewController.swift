a2569bee72c59be5c9368933c71f5912553db7bf
Alle,Sai Teja <s524964@ol23201m.nwmissouri.edu>
2016-03-15 21:24:29 -0500

    Created app launch screen

---
+++ Where's my Ride?/HomePageViewController.swift
@@ -0,0 +1,13 @@
+//
+//  HomePageViewController.swift
+//  Where's my Ride?
+//
+//  Created by Alle,Sai Teja on 3/15/16.
+//  Copyright © 2016 Alle,Sai Teja. All rights reserved.
+//
+
+import UIKit
+
+class HomePageViewController:UIViewController {
+    
+}
