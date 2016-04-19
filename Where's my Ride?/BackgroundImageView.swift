14524a8d221499d0f44713ac835eecc647ef9219
Student <s524975@ol23201m.nwmissouri.edu>
2016-04-19 17:51:03 -0500

    Added logo to app, launch screen, adding place holders for each text box. Changed background image for Driver. Removed few images which are not being used

--- Where's my Ride?/BackgroundImageView.swift
+++ Where's my Ride?/BackgroundImageView.swift
@@ -13,11 +13,11 @@ extension UIView {
         let width = UIScreen.mainScreen().bounds.size.width
         let height = UIScreen.mainScreen().bounds.size.height
         
-        let imageViewBackground = UIImageView(frame: CGRectMake(0, 0, width, height))
-        imageViewBackground.image = UIImage(named: "backgroundImage.jpg")
+        let imageViewBackground = UIImageView(frame: CGRectMake(0, 0, width, height*1.5))
+        imageViewBackground.image = UIImage(named: "bgHome.png")
         
         // you can change the content mode:
-        imageViewBackground.contentMode = UIViewContentMode.ScaleAspectFill
+        imageViewBackground.contentMode = UIViewContentMode.Center
         
         self.addSubview(imageViewBackground)
         self.sendSubviewToBack(imageViewBackground)
