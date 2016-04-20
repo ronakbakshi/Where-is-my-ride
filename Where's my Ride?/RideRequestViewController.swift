8ce5fb2094922874be1d8cffca8db6430d77865f
Student <s525059@ol10014m.nwmissouri.edu>
2016-04-19 20:11:10 -0500

    Worked on Request ride object

---
+++ Where's my Ride?/RideRequestViewController.swift
@@ -0,0 +1,26 @@
+//
+//  RideRequestViewController.swift
+//  Where's my Ride?
+//
+//  Created by Mogalipuvvu,Abhinaya Kamakshi Ammal on 4/19/16.
+//  Copyright © 2016 Alle,Sai Teja. All rights reserved.
+//
+
+import UIKit
+
+class RideRequestViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+}
