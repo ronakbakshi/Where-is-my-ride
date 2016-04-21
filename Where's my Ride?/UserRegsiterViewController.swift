7cd875e0763e335423956888c7b5113c1b9d72fd
Student <s524975@ch112016m.nwmissouri.edu>
2016-04-21 16:16:15 -0500

    added validations

--- Where's my Ride?/UserRegsiterViewController.swift
+++ Where's my Ride?/UserRegsiterViewController.swift
@@ -22,6 +22,9 @@ class UserRegsiterViewController: UIViewController {
     
     @IBOutlet weak var passwordTF: UITextField!
     
+    @IBOutlet weak var confirmPasswordTF: UITextField!
+    
+    
     var store : KCSAppdataStore!
     
 //    let contactRegex = "[0-9]"
@@ -56,12 +59,17 @@ class UserRegsiterViewController: UIViewController {
             withCompletionBlock: { (user: KCSUser!, errorOrNil: NSError!, result: KCSUserActionResult) -> Void in
                 if errorOrNil == nil {
                     //was successful!
-                    if self.userNameTF.text != "" && self.firstNameTF.text != "" && self.lastNameTF.text != "" && self.contactTF.text != "" && self.placeTF.text != "self." && self.passwordTF.text != ""{
+                    if self.userNameTF.text != "" && self.firstNameTF.text != "" && self.lastNameTF.text != "" && self.contactTF.text != "" && self.placeTF.text != "self." && self.passwordTF.text != "" && self.confirmPasswordTF.text != ""{
+                        
+                        if self.passwordTF.text == self.confirmPasswordTF.text{
                         
                         
                         self.displayAlertControllerWithTitle("Account Creation Successful", message: "User created. Welcome!")
                         let destinationVC:UserLoginViewController = self.navigationController?.storyboard?.instantiateViewControllerWithIdentifier("UserLoginViewController") as! UserLoginViewController
                         self.navigationController?.pushViewController(destinationVC, animated: true)
+                        }else{
+                            self.displayAlertControllerWithTitle("Create account failed", message: "Password and Confirm Password must be same.")
+                        }
                     }else{
                         //let message = errorOrNil.localizedDescription
                         self.displayAlertControllerWithTitle("Create account failed", message: "Please enter all the fields")
